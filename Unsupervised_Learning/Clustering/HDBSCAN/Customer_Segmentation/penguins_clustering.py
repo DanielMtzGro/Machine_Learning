463c40c4db9100d27fad4f749965090e09110daa
DanielMtzGro <danielmtzgro@hotmail.com>
2025-04-24 11:20:51 -0600

    Added HDBSCAN and 3D plot

--- Unsupervised_Learning/Clustering/HDBSCAN/Customer_Segmentation/penguins_clustering.py
+++ Unsupervised_Learning/Clustering/HDBSCAN/Customer_Segmentation/penguins_clustering.py
@@ -2,9 +2,35 @@ import numpy as np
 import hdbscan
 import matplotlib.pyplot as plt
 import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
 
-df = pd.read_csv("customer_segmentation.csv")
-print(df.head(3))
+# Read CSV
+df = pd.read_csv("./Datasets/penguins.csv")
+X = df[["flipper_length_mm", "culmen_length_mm", "culmen_depth_mm"]].dropna()
 
-plt.scatter(df["Income"], df["Year_Birth"], s=5)
+# Instantiate clusterer
+clusterer = hdbscan.HDBSCAN(min_cluster_size=10)
+
+# Fit with data
+labels = clusterer.fit_predict(X)
+
+# Plot the data in a 3D graph
+fig = plt.figure(figsize=(8,6))
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(
+    X["culmen_length_mm"],
+    X["flipper_length_mm"],
+    X["culmen_depth_mm"],
+    c=labels,
+    cmap="viridis",
+    s=5
+)
+
+# Plot configuration
+ax.set_title("Penguin clustering using HDBSCAN")
+ax.set_xlabel("Culmen length (mm)")
+ax.set_ylabel("Flipper length (mm)")
+ax.set_zlabel("Culmen depth (mm)")
 plt.show()
+

770cd91975415a22c3160c38babfef6a9179dc71
DanielMtzGro <danielmtzgro@hotmail.com>
2025-05-05 00:19:06 -0600

    Divide into train and test data

--- Supervised_Learning/Classification/Logistic_Regression/Fraud_Detection/log_reg_fraud_detection.py
+++ Supervised_Learning/Classification/Logistic_Regression/Fraud_Detection/log_reg_fraud_detection.py
@@ -18,6 +18,15 @@ X["electronics"] = X["merchant_type"] == "electronics"
 X["clothing"] = X["merchant_type"] == "clothing"
 X["others"] = X["merchant_type"] == "others"
 
-X.drop(["merchant_type"], axis=1)
+X["tablet"] = X["device_type"] == "tablet"
+X["desktop"] = X["device_type"] == "desktop"
+X["mobile"] = X["device_type"] == "mobile"
+
+X.drop(["merchant_type"], axis=1, inplace=True)
+X.drop(["device_type"], axis=1, inplace=True)
 
 print(X.head())
+
+# Divide into train and test data
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+

bed0deb03c6bd61654a5675bce952b33b8fb6952
DanielMtzGro <danielmtzgro@hotmail.com>
2025-05-05 01:09:23 -0600

    Changed treshold

--- Supervised_Learning/Classification/Logistic_Regression/Fraud_Detection/log_reg_fraud_detection.py
+++ Supervised_Learning/Classification/Logistic_Regression/Fraud_Detection/log_reg_fraud_detection.py
@@ -31,14 +31,14 @@ x_train_scaled = scaler.fit_transform(x_train)
 x_test_scaled = scaler.transform(x_test)
 
 # Create SMOTE
-smote = SMOTE(sampling_strategy=0.5, random_state=42)
+smote = SMOTE(sampling_strategy='auto', random_state=42)
 x_train_resampled, y_train_resampled = smote.fit_resample(x_train_scaled, y_train)
 
 
 #------------- TRAINING ---------------------
 
 # Create the model
-model = LogisticRegression(random_state=42, class_weight={0: 1, 1: 10})
+model = LogisticRegression(random_state=42, class_weight="balanced")
 
 # Fit the model with training data
 model.fit(x_train_resampled, y_train_resampled)
@@ -53,7 +53,7 @@ print(f"Model test accuracy: {model.score(x_test_scaled, y_test)}")
 y_pred_proba = model.predict_proba(x_test_scaled)[:, 1]  # Probabilidades de la clase 1
 
 # Ajustar el umbral
-threshold = 0.3  # Prueba con un umbral más bajo para capturar más fraudes
+threshold = 0.46  # Prueba con un umbral más bajo para capturar más fraudes
 y_pred_adjusted = (y_pred_proba > threshold).astype(int)
 
 # Mostrar el reporte de clasificación y la matriz de confusión
